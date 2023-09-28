4d50484db71faf0b6f9505dacc423221c34f55e7
Shchepin Daniel <shchepin.man@gmail.com>
2023-09-28 17:23:35 +0300

    mana system, play cards, but bad, need to fix it all

--- gameRoom.js
+++ gameRoom.js
@@ -1,6 +1,8 @@
 const turnTime = 10;
+const hp = 25;
 const cardsData = async () => await getCards();
-const array = async () => await [...(await cardsData()).cards, ...(await cardsData()).cards];
+const array = async () =>
+  await [...(await cardsData()).cards, ...(await cardsData()).cards];
 
 function randomIntFromInterval(min, max) {
   // min and max included
@@ -8,79 +10,159 @@ function randomIntFromInterval(min, max) {
 }
 
 async function getCards() {
-    try {
-      const filePath = "http://localhost:3000/cards.json";
-      const response = await fetch(filePath);
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      const cardData = await response.json();
-      return cardData;
-    } catch (error) {
-      console.error("Error fetching JSON:", error);
+  try {
+    const filePath = "http://localhost:3000/cards.json";
+    const response = await fetch(filePath);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    const cardData = await response.json();
+    return cardData;
+  } catch (error) {
+    console.error("Error fetching JSON:", error);
   }
-
+}
 
 async function gameRoom(firstSocket, secondSocket) {
+  let turn = true;
+  let timer = turnTime;
+  let firstHand = [];
+  let secondHand = [];
+  let firstMana = 1;
+  let secondMana = 1;
+  let enemyHp = hp;
+  let myHp = hp;
+  let firstManaLimit = 1;
+  let secondManaLimit = 1;
 
-    let turn = true;
-    let timer = turnTime;
-    let firstHand = [];
-    let secondHand = [];
+  for (let i = 0; i < 6; i++) {
+    const firstCard = await array();
+    firstHand.push(firstCard[randomIntFromInterval(0, firstCard.length - 1)]);
+    firstCard.splice(firstCard.indexOf(firstHand[i]), 1);
 
-    for (let i = 0; i < 6; i++) {
-      const firstCard = await array();
-      firstHand.push(firstCard[randomIntFromInterval(0, firstCard.length - 1)]);
-      firstCard.splice(firstCard.indexOf(firstHand[i]), 1);
-    
-      const secondCard = await array();
-      secondHand.push(secondCard[randomIntFromInterval(0, secondCard.length - 1)]);
-      secondCard.splice(secondCard.indexOf(secondHand[i]), 1);
+    const secondCard = await array();
+    secondHand.push(
+      secondCard[randomIntFromInterval(0, secondCard.length - 1)]
+    );
+    secondCard.splice(secondCard.indexOf(secondHand[i]), 1);
+  }
+
+  firstSocket.emit("game_start", {
+    turn: turn,
+    cards: array,
+    firstHand: firstHand,
+    secondHand: secondHand,
+    myHp: myHp,
+    enemyHp: enemyHp,
+    firstMana: firstMana,
+    secondMana: secondMana,
+  });
+  secondSocket.emit("game_start", {
+    turn: !turn,
+    cards: array,
+    firstHand: secondHand,
+    secondHand: firstHand,
+    myHp: enemyHp,
+    enemyHp: myHp,
+    firstMana: secondMana,
+    secondMana: firstMana,
+  });
+
+  firstSocket.emit("turn", {
+    turn: turn,
+    firstMana: firstMana,
+    secondMana: secondMana,
+  });
+  secondSocket.emit("turn", {
+    turn: !turn,
+    firstMana: secondMana,
+    secondMana: firstMana,
+  });
+
+  let timerI = setInterval(() => {
+    firstSocket.emit("timer", { timer: timer });
+    secondSocket.emit("timer", { timer: timer });
+    if (timer === 0) {
+      nextTurn();
+    } else {
+      timer--;
+    }
+  }, 1000);
+  secondSocket.request.session.data.timerI = timerI;
+  function nextTurn() {
+    firstSocket.emit("timer", { timer: timer });
+    secondSocket.emit("timer", { timer: timer });
+
+    if (firstManaLimit < 10) {
+      if (turn) {
+        secondManaLimit++;
+        secondMana = secondManaLimit;
+      }
+      else {
+        firstManaLimit++;
+        firstMana = firstManaLimit;
+      };
     }
 
-    firstSocket.emit('game_start', {turn: turn, cards: array, firstHand: firstHand, secondHand: secondHand});
-    secondSocket.emit('game_start', {turn: !turn, cards: array, firstHand: secondHand, secondHand: firstHand});
+    firstSocket.emit("mana", { firstMana: firstMana, secondMana: secondMana });
+    secondSocket.emit("mana", { firstMana: secondMana, secondMana: firstMana });
 
-    firstSocket.emit('turn', {turn: turn});
-    secondSocket.emit('turn', {turn: !turn});
+    turn = !turn;
+    firstSocket.emit("turn", {
+      turn: turn,
+      firstMana: firstMana,
+      secondMana: secondMana,
+    });
+    secondSocket.emit("turn", {
+      turn: !turn,
+      firstMana: secondMana,
+      secondMana: firstMana,
+    });
+    timer = turnTime;
+  }
 
-    let timerI = setInterval(() => {
-        firstSocket.emit('timer', {timer: timer});
-        secondSocket.emit('timer', {timer: timer});
-        if(timer === 0){
-            turn = !turn;
-            firstSocket.emit('turn', {turn: turn});
-            secondSocket.emit('turn', {turn: !turn});
-            timer = turnTime;
-        }
-        else{
-            timer--;
-        }
-    }, 1000);
-    secondSocket.request.session.data.timerI = timerI;
-    function nextTurn() {
-      firstSocket.emit('timer', {timer: timer});
-      secondSocket.emit('timer', {timer: timer});
-
-      turn = !turn;
-      firstSocket.emit('turn', {turn: turn});
-      secondSocket.emit('turn', {turn: !turn});
-      timer = turnTime;
+  firstSocket.on("next_turn", () => {
+    if (!turn) {
+      return;
     }
+    console.log(firstMana);
+    nextTurn();
+  });
 
-    firstSocket.on('next_turn', () => {
-        if (!turn) {
-            return;
-        }
-        nextTurn();
-    });
+  secondSocket.on("next_turn", () => {
+    if (turn) {
+      return;
+    }
+    console.log(secondMana);
+    nextTurn();
+  });
 
-    secondSocket.on('next_turn', () => {
-        if(turn){
-            return;
-        }
-        nextTurn();
-    });
+  firstSocket.on("play_card", (data) => {
+    if (!turn) {
+      return;
+    }
+    if (firstMana < data.cost) {
+      return;
+    }
+    firstMana -= data.cost;
+    firstSocket.emit("mana", { firstMana: firstMana, secondMana: secondMana });
+    secondSocket.emit("mana", { firstMana: secondMana, secondMana: firstMana });
+    firstHand.splice(firstHand.indexOf(data), 1);
+    secondSocket.emit("play_card", firstHand);
+  });
+
+  secondSocket.on("play_card", (data) => {
+    if (turn) {
+      return;
+    }
+    if (secondMana < data.cost) {
+      return;
+    }
+    secondMana -= data.cost;
+    firstSocket.emit("mana", { firstMana: firstMana, secondMana: secondMana });
+    secondSocket.emit("mana", { firstMana: secondMana, secondMana: firstMana });
+    secondHand.splice(secondHand.indexOf(data), 1);
+    firstSocket.emit("play_card", secondHand);
+  });
 }
 module.exports = gameRoom;
--- public/game.js
+++ public/game.js
@@ -3,17 +3,18 @@ const socket = io();
 let myLogin = "";
 let opLogin = "";
 let timer;
+let myMana;
 
 socket.on("waiting_for_opponent", () => {
   document.getElementById("waiting_for_opponent").style.display = "block";
 });
 
 socket.on("opponent_connected", (data) => {
-    opLogin = data.opLogin;
-    myLogin = data.myLogin;
-    document.getElementById("waiting_for_opponent").style.display = "none";
-    document.getElementById("opponent_login").style.display = "block";
-    document.getElementById("opponent").textContent = opLogin;
+  opLogin = data.opLogin;
+  myLogin = data.myLogin;
+  document.getElementById("waiting_for_opponent").style.display = "none";
+  document.getElementById("opponent_login").style.display = "block";
+  document.getElementById("opponent").textContent = opLogin;
 });
 
 socket.on("opponent_disconnected", () => {
@@ -39,36 +40,91 @@ socket.on("err_not_enough_money", () => {
 
 socket.on("game_start", (data) => {
   console.log(`game_start: ${data.turn}`);
+  myMana = data.firstMana;
   document.getElementById("game_board").style.display = "block";
   document.getElementById("loading-container").style.display = "none";
 
   data.firstHand.forEach((element) => {
-    const image = document.createElement("img");
-    console.log(element)
-    image.src = element.src;
-    document.getElementById("cards").appendChild(image);
+    renderCard(element); //! Нужно пофиксить, игрок вне своего хода может кидать карту если хватает маны
   });
 
-  data.secondHand.forEach((element) => {
-    const image = document.createElement("img");
-    image.src = element.src;
-    document.getElementById("enemy-cards").appendChild(image);
+  data.secondHand.forEach(() => {
+    renderCardBack();
   });
+
+  document.getElementById("enemy-name").innerText = opLogin;
+  document.getElementById("my-name").innerText = myLogin;
+
+  document.getElementById("enemy-portrait").src = "/avatar/" + opLogin + ".jpg"
+  document.getElementById("my-portrait").src = "/avatar/" + myLogin + ".jpg"
+
+  document.getElementById("enemy-hp").innerText = data.enemyHp + " HP";
+  document.getElementById("my-hp").innerText = data.myHp + " HP";
+  document.getElementById("enemy-mana").innerText = data.secondMana + " Mana";
+  document.getElementById("my-mana").innerText = data.firstMana + " Mana";
 });
 
 socket.on("timer", (data) => {
-    document.getElementById("timer").innerText = data.timer;
-    console.log(`timer: ${data.timer}`);
+  document.getElementById("timer").innerText = data.timer;
 });
 
 socket.on("turn", (data) => {
-    if (data.turn) {
-        document.getElementById("turn_nickname").innerText = myLogin + " turn";
-    } else {
-        document.getElementById("turn_nickname").innerText = opLogin + " turn";
-    }
+  if (data.turn) {
+    document.getElementById("turn_nickname").innerText = myLogin + " turn";
+    document.getElementById("enemy-mana").innerText = data.secondMana + " Mana";
+    document.getElementById("my-mana").innerText = data.firstMana + " Mana";
+    
+  } else {
+    document.getElementById("turn_nickname").innerText = opLogin + " turn";
+    document.getElementById("enemy-mana").innerText = data.secondMana + " Mana";
+    document.getElementById("my-mana").innerText = data.firstMana + " Mana";
+    
+  }
+});
+
+socket.on("mana", (data) => {
+  myMana = data.firstMana;
+  document.getElementById("my-mana").innerText = data.firstMana + " Mana";
+  document.getElementById("enemy-mana").innerText = data.secondMana + " Mana";
 });
 
 function passOnClick() {
-    socket.emit('next_turn');
+  socket.emit("next_turn");
+}
+
+const playCard = (card) => {
+  
+  socket.emit("play_card", card);
+  
+}
+
+socket.on("play_card", (data) => {
+  document.getElementById("enemy-cards").innerHTML = "";
+  data.forEach(() => {
+    renderCardBack();
+  });
+});
+
+const renderCard = (card) => {
+  const image = document.createElement("img");
+  console.log(card);
+  image.src = card.src;
+  image.addEventListener("click", (event) => {
+    console.log(myMana + " " + card.cost)
+    if (myMana < card.cost) {
+      return;
+    }
+    else {  
+      //! animation for playing card
+      playCard(card);
+      event.currentTarget.remove();
+    }
+  });
+  document.getElementById("cards").appendChild(image);
+}
+
+const renderCardBack = () => {
+  const image = document.createElement("img");
+  image.src = "/images/card-back.png";
+  document.getElementById("enemy-cards").appendChild(image);
 }
